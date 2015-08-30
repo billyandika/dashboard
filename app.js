58820925350b9e8cb9e4fc737e5a4c06448cc009
Billy Andika <bandika@ucsd.edu>
2015-08-30 15:36:12 +0700

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var index = require('./routes/index');
 
 // Connect to the Mongo database, whether locally or on Heroku
 // MAKE SURE TO CHANGE THE NAME FROM 'lab7' TO ... IN OTHER PROJECTS
-var local_database_name = 'lab7';
+var local_database_name = 'customerdb';
 var local_database_uri  = 'mongodb://localhost/' + local_database_name
 var database_uri = process.env.MONGOLAB_URI || local_database_uri
 mongoose.connect(database_uri);
