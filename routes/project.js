970b41c22b24196470afd1162c64b0268cae2b04
Billy Andika <bandika@ucsd.edu>
2015-08-30 13:06:54 +0700

    update

--- routes/project.js
+++ routes/project.js
@@ -6,9 +6,6 @@ exports.projectInfo = function(req, res) {
   models.Project.find({"_id": projectID})
   .exec(afterQuery);
 
-  // query for the specific project and
-  // call the following callback
-
   function afterQuery(err, projects) {
     if(err) console.log(err);
     res.json(projects[0]);
