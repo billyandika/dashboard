13300b22ce48f7ca53e2a01e96279dda161c222a
Billy Andika <bandika@ucsd.edu>
2015-08-31 08:21:25 +0700

    update

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -28,6 +28,23 @@ function initializePage() {
 		// issue the GET request
 		$.get(url_call, addCustomerDetails);
 	});
+
+	// $('#approveButton').click(function(e) {
+	// 	console.log('clicked');
+	// 	var title = $('#new-project-form #title').val();
+	// 	var image_url = $('#new-project-form #image_url').val();
+	// 	var date = $('#new-project-form #date').val();
+	// 	var summary = $('#new-project-form #summary').val();
+	// 	var json = {
+	// 		'project_title': title,
+	// 		'image_url': image_url,
+	// 		'date':  date,
+	// 		'summary': summary
+	// 	};
+	// 	$.post('/customer/new', json, function() {
+	// 		window.location.href = '/'; // reload the page
+	// 	});
+	// });
 }
 
 
--- routes/customer.js
+++ routes/customer.js
@@ -14,3 +14,43 @@ exports.customerInfo = function(req, res) {
     res.json(customers[0]);
   }
 }
+
+exports.addCustomer = function(req, res) {
+  var form_data = req.body;
+  // console.log(form_data);
+
+  // make a new Customer and save it to the DB
+  // YOU MUST send an OK response w/ res.send();
+  var CustomerItem = new models.Customer({
+    "title": form_data.customer_title,
+    "image": form_data.image_url,
+    "date": form_data.date,
+    "summary": form_data.summary
+  });
+
+  CustomerItem.save(afterSaving);
+
+  function afterSaving(err) {
+    if(err) { console.log(err); res.send(500); }
+    res.send();
+  }
+}
+
+// exports.updateCustomer = function(req, res) {
+
+// }
+
+exports.deleteCustomer = function(req, res) {
+  var customerID = req.params.id;
+
+  // find the customer and remove it
+  // YOU MUST send an OK response w/ res.send();
+  models.Customer.find({"_id": customerID})
+  .remove()
+  .exec(deleteCallback);
+
+  function deleteCallback(err) {
+    if(err) {console.log(err)};
+    res.send();
+  }
+}
